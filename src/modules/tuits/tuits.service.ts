cb0c17abd29ec845e4f9c7bd47e5450750795712
jhhg04 <jhhg04@gmail.com>
2023-12-13 22:46:46 -0500

    first commit

---
+++ src/modules/tuits/tuits.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Tuit } from './tuit.entity';
+import { CreateTuitDto, UpdateTuitDto } from './dto';
+
+@Injectable()
+export class TuitsService {
+  private tuits: Tuit[] = [
+    {
+      id: '1',
+      message: 'Hello world from Nest.js',
+    },
+  ];
+
+  getTuits(): Tuit[] {
+    return this.tuits;
+  }
+
+  getTuit(id: string): Tuit {
+    return this.tuits.find((item) => item.id === id);
+  }
+
+  createTuit({ message }: CreateTuitDto) {
+    this.tuits.push({
+      id: (Math.floor(Math.random() * 2000) + 1).toString(),
+      message,
+    });
+  }
+
+  updateTuit(id: string, { message }: UpdateTuitDto) {
+    const tuit: Tuit = this.getTuit(id);
+    tuit.message = message;
+    return tuit;
+  }
+
+  removeTuit(id: string) {
+    const index = this.tuits.findIndex((tuit) => tuit.id === id);
+    if (index >= 0) {
+      this.tuits.splice(index, 1);
+    }
+  }
+}
